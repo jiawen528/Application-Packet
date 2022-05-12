bcdb71ec7e8846be91058b132be9e7c8fade9e3e
Rose8339 <101231134+jiawen528@users.noreply.github.com>
2022-05-12 20:02:42 +0800

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import random
+
+answer = random.randint(1, 100)
+counter = 0
+while True:
+    counter += 1
+    number = int(input('請輸入: '))
+    if number < answer:
+        print('大一點')
+    elif number > answer:
+        print('小一點')
+    else:
+        print('恭喜你猜對了!')
+        break
+print('你總共猜了%d次' % counter)
+if counter > 7:
+    print('你的智商餘額明顯不足')
+
+
+# In[ ]:
+
+
+
+
